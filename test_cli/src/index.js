477275f7a23f81bc7d64a64ceb572a9f9e96e8c0
liyuaaaa11 <xsanjin0706@gmail.com>
2025-01-20 00:08:22 +0800

    fix: 解决无法执行自定义命令等问题

--- test_cli/src/index.js
+++ test_cli/src/index.js
@@ -1,11 +1,14 @@
-#!/user/bin/env node
+#!/usr/bin/env node
 // 告诉系统用node来执行这个文件
 import { program } from 'commander';
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // 获取当前目录下的package.json文件
-let json = fs.readFileSync(path.resolve(__dirname, './package.json'), 'utf-8');
-json.version = '0.0.1';
+let json = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8');
+json = JSON.parse(json)
 console.log('Hello, world!');
 // 定义命令行操作
 // 设置版本号
