b1d2d93a36037fd1c6c649927d7d4065d2aa517f
liyuaaaa11 <xsanjin0706@gmail.com>
2025-01-26 00:54:28 +0800

    feat: gzip解压文件及deflate压缩/解压

--- global/zlib/README.md
+++ global/zlib/README.md
@@ -1,7 +1,6 @@
 ### zlib模块
 * 支持数据压缩和解压缩，支持多种压缩算法，包括Deflate、Gzip和Raw Deflate。
-1. gzip压缩
-引入zlib模块，将index.txt压缩为index.txt.gz
+1. 前期准备
 ```
 improt zlib from 'node:zlib'
 improt fs from 'node:fs'
@@ -10,8 +9,17 @@ improt {fileURLToPath} from 'node:url'
 
 const __filename = fileURLToPath(improt.mate.url)
 const __dirname = path.dirname(__filename)
-
+```
+2. gzip压缩/解压
+**createGzip 压缩文件**
+```
 const readStream = createReadStream(path.resolve(__dirname, 'index.txt'))
 const writeStream = createWriteStream(path.resolve(__diename, 'index.txt.gz'))
 readStream.pipe(zlib.createGzip()).pipe(writeStream)
+```
+**createGunzip 解压文件**
+```
+const readGunStream = createReadStream(path.resolve(__dirname, 'index.txt'))
+const writeGunStream = createWriteStream(path.resolve(__diename, 'index.txt.gz'))
+readGunStream.pipe(zlib.createGzip()).pipe(writeGunStream)
 ```
--- global/zlib/index.js
+++ global/zlib/index.js
@@ -5,6 +5,13 @@ import { fileURLToPath } from 'node:url'
 const __filename = fileURLToPath(import.meta.url) // 获取文件的解析路径
 const __dirname = path.dirname(__filename) //获取目录名称
 
-const readStream = fs.createReadStream(path.resolve(__dirname, 'index.txt'))
-const writeStream = fs.createWriteStream(path.resolve(__dirname, 'index.txt.gz'))
-readStream.pipe(zlib.createGzip()).pipe(writeStream)
+// 1. gzip压缩
+// 压缩文件
+// const readGzipStream = fs.createReadStream(path.resolve(__dirname, 'index.txt'))
+// const writeGzipStream = fs.createWriteStream(path.resolve(__dirname, 'index.txt.gz'))
+// readGzipStream.pipe(zlib.createGzip()).pipe(writeGzipStream)
+
+// 解压文件
+const readGunStream = fs.createReadStream(path.resolve(__dirname, 'index.txt.gz'))
+const writeGunStream = fs.createWriteStream(path.resolve(__dirname, 'index2.txt'))
+readGunStream.pipe(zlib.createGunzip()).pipe(writeGunStream)
