b5f47825002de635ab05f59dae6507a8c3c11644
liyuaaaa11 <xsanjin0706@gmail.com>
2025-02-02 12:03:55 +0800

    perf: 删除冗余代码

--- global/http/proxy/index.js
+++ global/http/proxy/index.js
@@ -11,23 +11,11 @@ const html = fs.readFileSync(path.resolve(__dirname, './../index.html'))
 
 // 检测代理端口是否存在
 function proxyCheck(pathname) {
-  // const config = fs.readFileSync(path.resolve(__dirname, './../xsanjin.config.js'))
   console.log(config.default.serve)
   const proxyList = Object.keys(config.default.serve.proxy)
   console.log(config,proxyList)
   console.log('当前端口是否存在', proxyList.includes(pathname))
   return proxyList.includes(pathname)
-}
-
-// 检测是否存在该用户
-function userCheck(data) {
-  const { userlist } = path.resolve(__dirname, './user.json')
-  console.log(data, userlist)
-  if (userlist.length === 0) return false
-  const user = userlist.find(item => {
-    return data.name === item.name && item.password == data.password
-  })
-  return user
 }
 
 http.createServer((req, res) => {
@@ -43,14 +31,10 @@ http.createServer((req, res) => {
     req.on('data', chunk => {
       data +=chunk
     })
-    if (userCheck(data)) {
-      req.on('end', () => {
-        res.end(data)
-      })
-    } else {
+    req.on('end', () => {
       res.statusCode = 200
-      res.end('不存在该用户')
-    }
+      res.end(data)
+    })
   } else {
     res.statusCode = 404
     res.end('404～ 当前暂无此功能，待后续研发！')
