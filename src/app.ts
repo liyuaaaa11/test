d983ff37f80f6f461a064e3ac582d7c9581b4518
liyuaaaa11 <xsanjin0706@gmail.com>
2025-06-07 22:09:45 +0800

    feat: 配置跨域请求限制

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import { InversifyExpressServer } from 'inversify-express-utils';
 import { Container } from 'inversify';
 // 引入express
 import express from 'express';
+// 配置跨域 引入cors
+const cors = require('cors')
 // 引入prisma/client  封装数据操作
 import { PrismaClient } from '@prisma/client';
 // 引入封装好的数据库模块
@@ -19,6 +21,7 @@ import { UserService } from './user/service.js';
 // 引入teachShare模块
 import { TeachShareController } from './teachShare/controller.js';
 import { TeachShareService } from './teachShare/service.js';
+import exp from 'constants';
 
 const container = new Container();
 // 注入user模块
@@ -39,13 +42,25 @@ container.bind<PrismaClient>('PrismaClient').toFactory(() => {
 container.bind(PrismaDB).to(PrismaDB);
 container.bind(JWT).to(JWT)
 
+// 配置全局cors
+const corsOptions = {
+  origin: 'http://localhost:3000', // 允许的来源
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 允许的HTTP方法
+  allowedHeaders: ['Content-Type', 'Authorization'], // 允许的请求头
+  exposedHeaders: ['Content-Length', 'X-Requested-With', 'Authorization'], // 允许暴露的响应头
+  credentials: true, // 允许携带凭证
+  maxAge: 86400 // 预检请求的缓存时间，单位为秒
+};
+
 const server = new InversifyExpressServer(container);
 // 编写中间键
 server.setConfig((app) => {
   // 支持接收json数据格式
   app.use(express.json());
+  // 使用cors中间件
+  app.use(cors(corsOptions));
   // 读取jwt内部init方法并与express关联
-  app.use(container.get(JWT).init())
+  app.use(container.get(JWT).init());
 });
 const app = server.build();
 
